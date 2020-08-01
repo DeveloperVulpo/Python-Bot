cea43c09355aff61b260fed5b5e3377d10b73232
Boa <rubberboa01@gmail.com>
2020-08-01 15:18:13 -0700

    useless parenthesis

--- cogs/fun.py
+++ cogs/fun.py
@@ -30,13 +30,13 @@ class Fun(commands.Cog):
     async def rps(self, ctx, l: str):
         choices = ["rock", "paper", "scissors"]
         chosen = random.choice(choices)
-        if(l == "rock" and chosen == "paper"):
+        if l == "rock" and chosen == "paper":
             await ctx.send(f"I win, I choosen {chosen}")
-        elif(l == "paper" and chosen == "scissors"):
+        elif l == "paper" and chosen == "scissors":
             await ctx.send(f"I win, I choosen {chosen}")
-        elif(l == "scissors" and chosen == "rock"):
+        elif l == "scissors" and chosen == "rock":
             await ctx.send(f"I win, I choosen {chosen}")
-        elif(l == chosen ):
+        elif l == chosen:
             await ctx.send(f"We Tied, I choosen {chosen}")
         else:
             await ctx.send(f"I have lost, I choosen {chosen}")
